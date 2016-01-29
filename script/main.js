4c01062748e35544f17a85c18919ce8b93578c34
dogmax296 <ivanov1maxim@gmail.com>
2016-01-29 20:10:26 +0200

    Start

--- script/main.js
+++ script/main.js
@@ -2,5 +2,9 @@ $('.slideshow__carousel').owlCarousel({
     loop:true,
     margin:0,
     nav:false,
-    items:1
+    items:1,
+    autoplay:true,
+    autoplayHoverPause:true,
+    autoplayTimeout:4000
+
 })
