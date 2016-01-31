83621811458c9d7d5525988867e7773801df34f1
dogmax296 <ivanov1maxim@gmail.com>
2016-01-31 21:36:29 +0200

    Markup and Css styles  - done

--- script/main.js
+++ script/main.js
@@ -8,3 +8,24 @@ $('.slideshow__carousel').owlCarousel({
     autoplayTimeout:4000
 
 })
+
+// $(document).ready(function(){
+//     /* Get iframe src attribute value i.e. YouTube video url
+//     and store it in a variable */
+//     var url = $("#video").attr('src');
+    
+//     /* Assign empty url value to the iframe src attribute when
+//     modal hide, which stop the video playing */
+//     $("#modal-video").on('hide.bs.modal', function(){
+//         $("#video").attr('src', '');
+//     });
+    
+//     /* Assign the initially stored url back to the iframe src
+//     attribute when modal is displayed again */
+//     $("#modal-video").on('show.bs.modal', function(){
+//         $("#video").attr('src', url);
+//     });
+// });
+
+// document.write(new Date().getFullYear())
+
